14628d5b8f750c540dd5fee8eb77536e1632bcf5
Prathamesh bhatte <prathamb142@gmail.com>
2025-03-27 10:35:41 +0530

    Made few changes. tried NLP and gemini

--- lib/Home/new_homepage.dart
+++ lib/Home/new_homepage.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(RecipeApp());
-}
 
 class RecipeApp extends StatelessWidget {
+  const RecipeApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
