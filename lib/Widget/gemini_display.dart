e204a63092d1cb47a3d4f27a53e6fe3c65cace0f
Prathamesh bhatte <prathamb142@gmail.com>
2025-04-01 22:00:11 +0530

    added a file to display gemini output

---
+++ lib/Widget/gemini_display.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RecipeDetailPage extends StatelessWidget {
+  final String query;
+  final List<Widget> formattedRecipe;
+
+  const RecipeDetailPage({Key? key, required this.query, required this.formattedRecipe}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Recipe for $query")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: formattedRecipe, // Display formatted widgets
+          ),
+        ),
+      ),
+    );
+  }
+}
